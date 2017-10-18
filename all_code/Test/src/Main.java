895d23930d019eea7f145d3e5dd340cf3d814e2c
varocarbas <contact@customsolvers.com>
2017-10-18 21:44:51 +0200

    NumberParser 1.0.8.5 & UnitParser 1.0.9.1.

--- all_code/Test/src/Main.java
+++ all_code/Test/src/Main.java
@@ -4,8 +4,10 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		//UnitParser tests.
-	    UnitParser.StartTest();		
+    	//UnitParser tests.
+	    //UnitParser.StartTest();	
+		
+    	//NumberParser tests.
+	    NumberParser.StartTest();
 	}
 }
-
